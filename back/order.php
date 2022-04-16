e207c0f6b2b04c7d0c72ae516edaba3b4c662f5b
teatimehr13 <a5432158086@gmail.com>
2022-04-16 16:21:51 +0800

    製作後台的訂單列表

--- back/order.php
+++ back/order.php
@@ -1,1 +1,27 @@
-order
+<h1 class="ct">訂單管理</h1>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>訂單號</td>
+        <td>金額</td>
+        <td>會員帳號</td>
+        <td>姓名</td>
+        <td>下單時間</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp ct">
+        <?php
+        $ords = $Ord->all();
+        foreach ($ords as $ord) {
+        ?>
+            <td><a href="?do=detail&id=<?=$ord['id'];?>"><?=$ord['no'];?></a></td>
+            <td><?=$ord['total'];?></td>
+            <td><?=$ord['acc'];?></td>
+            <td><?=$ord['name'];?></td>
+            <td><?=date("Y-m-d",strtotime($ord['orddate']));?></td> <!--轉成秒數再轉date-->
+            <td><button onclick="del('ord',<?=$ord['id'];?>)">刪除</button></td>
+        <?php
+        }
+        ?>
+    </tr>
+</table>
